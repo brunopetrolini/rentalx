5f24e6556dc49ce312c3d5e8ca6692a5d0e8abad
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-03-24 21:11:25 -0300

    add session authentication with JWT

---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUseController = new AuthenticateUserController();
+authenticateRoutes.post("/sessions", authenticateUseController.handle);
+
+export { authenticateRoutes };
