2a2629369c4e6d61d429d6a6ea9a67d3c5ba67b6
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-03-29 22:24:51 -0300

    refactored folders architecture

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,13 +3,12 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
+import { router } from "@shared/infra/http/routes";
 
-import { router } from "./routes";
-import swaggerFile from "./swagger.json";
+import swaggerFile from "../../../swagger.json";
 
-import "./database";
-import "./shared/container";
+import "@shared/infra/typeorm";
 
 const app = express();
 
