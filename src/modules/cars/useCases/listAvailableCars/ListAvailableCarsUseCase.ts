5b6f06ca8bf22fccfdbe9c194716262f1f1e1c84
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-02 17:41:26 -0300

    add list all available cars route

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
@@ -7,8 +9,12 @@ interface IRequest {
   name?: string;
 }
 
+@injectable()
 class ListAvailableCarsUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
 
   async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
     const cars = await this.carsRepository.findAvailable(
