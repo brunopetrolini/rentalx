be82f8f1b89298c6588c9110dc2c79917210d470
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-02 17:16:39 -0300

    ensure to list all available cars by brand

--- src/modules/cars/useCases/listCars/listCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/listCarsUseCase.spec.ts
@@ -29,16 +29,32 @@ describe("List cars", () => {
 
   it("Should be able to list all available cars by name", async () => {
     const car = await carsRepository.create({
-      name: "other_car_name",
-      description: "other_car_description",
+      name: "1_car_name",
+      description: "1_car_description",
       daily_rate: 110,
-      license_plate: "other_car_plate",
+      license_plate: "1_car_plate",
       fine_amount: 60,
-      brand: "other_car_brand",
+      brand: "1_car_brand",
       category_id: "category_id",
     });
 
-    const cars = await listCarsUseCase.execute({ name: "other_car_name" });
+    const cars = await listCarsUseCase.execute({ name: "1_car_name" });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("Shold be able to list all available cars by brand", async () => {
+    const car = await carsRepository.create({
+      name: "2_car_name",
+      description: "1_car_description",
+      daily_rate: 110,
+      license_plate: "2_car_plate",
+      fine_amount: 60,
+      brand: "2_car_brand",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({ brand: "2_car_brand" });
 
     expect(cars).toEqual([car]);
   });
