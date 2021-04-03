ad2e2f14909fce8924a91078254a2bbb4be45122
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-02 22:11:35 -0300

    add CarImagesRepository

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImage/UploadCarImagesController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
+
+const uploadImages = multer(uploadConfig.upload("./temp/cars"));
 
 const createCarController = new CreateCarController();
 carsRoutes.post(
@@ -26,6 +31,15 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
+);
+
+const uploadCarImagesController = new UploadCarImagesController();
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadImages.array("images"),
+  uploadCarImagesController.handle
 );
 
 export { carsRoutes };
