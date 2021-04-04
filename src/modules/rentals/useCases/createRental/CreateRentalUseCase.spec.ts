d6cf21d5596bee2d5e6eedc801497aab53ee12be
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-04 17:16:09 -0300

    ensure to not create a rental if there is another to the same user

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
@@ -20,5 +21,21 @@ describe("Create Rental", () => {
 
     expect(rental).toHaveProperty("id");
     expect(rental).toHaveProperty("start_date");
+  });
+
+  it("Should not be able to create a rental if there's another open to the same user", () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "any_user_id",
+        car_id: "any_car_id",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "any_user_id",
+        car_id: "other_car_id",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
