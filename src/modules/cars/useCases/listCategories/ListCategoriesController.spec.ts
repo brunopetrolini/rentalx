fda954dd82e9fbbf1b541bd73ead3bf4ab76a39e
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-05 21:15:53 -0300

    ensure ListCategoriesController returns a category

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,53 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("Create category controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash("master", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", driver_license, created_at)
+        VALUES('${id}', 'admin', 'admin@rentalx.com.br', '${password}', true, 'XXXXXX', 'now()')`
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to list all categories", async () => {
+    const responseToken = await request(app)
+      .post("/sessions")
+      .send({ email: "admin@rentalx.com.br", password: "master" });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({
+        Authorization: `baerer ${token}`,
+      });
+
+    const response = await request(app).get("/categories");
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("id");
+  });
+});
