1db1eee71f1f06ed0c192dc615d6e84924bcca0d
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-13 21:18:02 -0300

    add refresh token generation on AuthenticateUseCase

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "484f1c5d540e55294143e3d476346509",
+  secret_refresh_token: "873f160591daf262ea7276f05ed6abee",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
