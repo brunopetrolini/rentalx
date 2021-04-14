3dd643d760b22c244cd078c9231e2fc51157af20
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-13 21:53:56 -0300

    add RefreshTokenController

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -5,9 +5,16 @@ import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const token =
+      request.body.token ||
+      request.headers["x-access-token"] ||
+      request.query.token;
+
     const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
 
-    return response.status(200).send();
+    const refreshToken = await refreshTokenUseCase.execute(token);
+
+    return response.status(200).json(refreshToken);
   }
 }
 
