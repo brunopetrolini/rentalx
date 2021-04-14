afda7c03aeca975eb401f5bee948f9b3a0c30214
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-13 21:58:55 -0300

    add refres token route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
 const authenticateUseController = new AuthenticateUserController();
 authenticateRoutes.post("/sessions", authenticateUseController.handle);
+
+const refreshTokenControler = new RefreshTokenController();
+authenticateRoutes.post("/refresh-token", refreshTokenControler.handle);
 
 export { authenticateRoutes };
