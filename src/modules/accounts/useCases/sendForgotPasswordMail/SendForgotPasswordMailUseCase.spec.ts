a7a0f64936f78644a26191ba7d23ff982f325268
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-19 21:03:23 -0300

    ensure SendForgotPasswordMailUseCase send a email to user

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayJSDateProvider } from "@shared/container/providers/DateProvider/implementations/DayJSDateProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
+
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
+
+let usersRepository: UsersRepositoryInMemory;
+let usersTokensRepository: UsersTokensRepositoryInMemory;
+let dateProvider: DayJSDateProvider;
+let mailProvider: MailProviderInMemory;
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+
+describe("Send Forgot Email", () => {
+  beforeEach(() => {
+    usersRepository = new UsersRepositoryInMemory();
+    usersTokensRepository = new UsersTokensRepositoryInMemory();
+    dateProvider = new DayJSDateProvider();
+    mailProvider = new MailProviderInMemory();
+    sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+      usersRepository,
+      usersTokensRepository,
+      dateProvider,
+      mailProvider
+    );
+  });
+
+  it("Should be able to send a forgot password email to user", async () => {
+    const sendMail = spyOn(mailProvider, "sendMail");
+
+    await usersRepository.create({
+      name: "Julian Francis",
+      email: "zesihjuc@foj.ge",
+      password: "U2RkFd11",
+      driver_license: "792463290",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("zesihjuc@foj.ge");
+
+    expect(sendMail).toHaveBeenCalledTimes(1);
+  });
+});
