e2423915e34231180e47eb08ba1be48335b31b87
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-27 20:25:45 -0300

    update upload method for supports aws S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,19 +2,16 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
-export default {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  upload(folder: string) {
-    return {
-      storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", folder),
-        filename: (request, file, callback) => {
-          const fileHash = crypto.randomBytes(16).toString("hex");
-          const fileName = `${fileHash}-${file.originalname}`;
+const tempFolder = resolve(__dirname, "..", "..", "temp");
 
-          return callback(null, fileName);
-        },
-      }),
-    };
-  },
+export default {
+  storage: multer.diskStorage({
+    destination: tempFolder,
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(16).toString("hex");
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
 };
---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,13 @@
+import { IStorageProvider } from "../IStorageProvider";
+
+class LocalStorageProvider implements IStorageProvider {
+  save(file: string): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+
+  delete(file: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export { LocalStorageProvider };
