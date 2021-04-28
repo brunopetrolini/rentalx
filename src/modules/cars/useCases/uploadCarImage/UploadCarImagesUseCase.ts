162f8fb659899ed6c5014b8e60437b480668ff88
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-28 19:42:12 -0300

    update UploadCarImageUseCase to use S3

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -11,12 +12,16 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImageRepository")
-    private carsImageRepository: ICarsImagesRepository
+    private carsImageRepository: ICarsImagesRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carsImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
