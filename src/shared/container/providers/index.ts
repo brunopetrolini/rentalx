a0a062199bebbfef3d99e9ff528876d28e30c5cf
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-05-10 19:51:19 -0300

    add SESMailProvider container

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,20 +4,26 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayJSDateProvider } from "./DateProvider/implementations/DayJSDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { SESMailProvider } from "./MailProvider/implementations/SESMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayJSDateProvider);
 
+const mailProvider = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  new EtherealMailProvider()
+  mailProvider[process.env.MAIL_PROVIDER]
 );
 
 const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
+  local: container.resolve(LocalStorageProvider),
+  s3: container.resolve(S3StorageProvider),
 };
 
 container.registerSingleton<IStorageProvider>(
